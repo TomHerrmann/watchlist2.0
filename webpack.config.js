5504a0b80bb1643c6c90b1e582a02fc08daef88f
Tom Herrmann <tomherrmanndev@gmail.com>
2019-12-16 21:52:07 -0500

    Webpack setup continued

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,29 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
 }
