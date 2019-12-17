12a2815cc1f6ebca1e3a38865764f4cb625d63b9
Tom Herrmann <tomherrmanndev@gmail.com>
2019-12-16 22:53:28 -0500

    Setup hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
-const React = require('react');
+const React, { useState } = require('react');
 const ReactDom = require('react-dom');
 
-export default module(hot)(App)
+const watchList = ({ title, index }) => {
+  <div className="media">
+    {media.text}
+  </div>
+}
+
+const App = () => {
+  const [watchList, setWatchList] = useState([
+    {
+      text: 'test react hooks',
+      isComple: false,
+    },
+    {
+      text: 'this is hooks hooking',
+      isComple: false,
+    },
+    {
+      text: 'just look at it hook',
+      isComple: false,
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="watch-list">
+        {watchList.forEach((elem, index) => {
+          <Media key={index} index={index} media={elem} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App;
