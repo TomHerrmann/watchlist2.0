d2524a3919f810f1e01cf9dda29c00e35acacc49
Tom Herrmann <tomherrmanndev@gmail.com>
2020-01-27 16:12:19 -0500

    Temp remove hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
 import React, { Component, useState } from 'react';
 import './style.css'
 
-const watchList = ({ title, index }) => {
-  <div className="media">
-    <h2>watchList</h2>
-  </div>
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
 
-const App = () => {
-  const { watchList, setWatchList } = React.useState([
-    {
-      text: 'test react hooks',
-      isComple: false,
-    },
-    {
-      text: 'this is hooks hooking',
-      isComple: false,
-    },
-    {
-      text: 'just look at it hook',
-      isComple: false,
     }
-  ]);
+  }
 
-  return (
-    <div className="app">
-      <div className="watch-list">
-        <h1>APPPPPPPPP</h1>
-        {watchList.forEach((elem, index) => {
-          <Media key={index} index={index} media={elem} />
-        })}
+  render() {
+    return (
+      <div className="app" >
+        <div className="watch-list">
+          <h1>APPPPPPPPP</h1>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
